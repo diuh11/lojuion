e5b58037406bbd7ce3cabfd4374bdea4bb8e87e8
diuh11 <arroflin3@gmail.com>
2026-09-23 19:08:07 +0200

    Update merge.py

--- merge.py
+++ merge.py
@@ -1,4 +1,29 @@
+import requests
+
+URLS = [
+    "https://pastebin.com/raw/hV2z2e9g",
+    "https://raw.githubusercontent.com/Free-TV/IPTV/refs/heads/master/playlists/playlist_spain.m3u8",
+    "https://www.apsattv.com/rakuten_es.m3u"
+]
+
+salida = ["#EXTM3U"]
+
+for url in URLS:
+    try:
+        print(f"Descargando {url}")
+        contenido = requests.get(url, timeout=30).text
+
+        lineas = contenido.splitlines()
+
+        if lineas and lineas[0].startswith("#EXTM3U"):
+            lineas = lineas[1:]
+
+        salida.extend(lineas)
+
+    except Exception as e:
+        print(f"Error: {e}")
+
 with open("lista.m3u", "w", encoding="utf-8") as f:
-    f.write("#EXTM3U\n")
+    f.write("\n".join(salida))
 
 print("Lista creada correctamente")
