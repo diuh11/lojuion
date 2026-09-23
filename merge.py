a85dbc076ab99616fe285ac1e07b932e96ef0e55
diuh11 <arroflin3@gmail.com>
2026-09-23 20:12:58 +0200

    Update merge.py

--- merge.py
+++ merge.py
@@ -1,9 +1,23 @@
 import requests
+import re
 
 URLS = [
     "https://pastebin.com/raw/hV2z2e9g",
     "https://raw.githubusercontent.com/Free-TV/IPTV/refs/heads/master/playlists/playlist_spain.m3u8",
     "https://www.apsattv.com/rakuten_es.m3u"
+]
+
+EXCLUIR = [
+    "portugal",
+    "mexico",
+    "méxico",
+    "polsat",
+    "vodafone ru",
+    "now tv",
+    "viasport",
+    "suecia",
+    "noruega",
+    "worldcup club"
 ]
 
 salida = [
@@ -13,14 +27,47 @@ salida = [
 for url in URLS:
     try:
         print(f"Descargando {url}")
-        contenido = requests.get(url, timeout=30).text
 
+        contenido = requests.get(url, timeout=30).text
         lineas = contenido.splitlines()
 
         if lineas and lineas[0].startswith("#EXTM3U"):
             lineas = lineas[1:]
 
-        salida.extend(lineas)
+        i = 0
+
+        while i < len(lineas):
+
+            if lineas[i].startswith("#EXTINF"):
+
+                extinf = lineas[i]
+
+                enlace = ""
+                if i + 1 < len(lineas):
+                    enlace = lineas[i + 1]
+
+                grupo = ""
+
+                match = re.search(
+                    r'group-title="([^"]*)"',
+                    extinf,
+                    re.IGNORECASE
+                )
+
+                if match:
+                    grupo = match.group(1).lower()
+
+                if any(x in grupo for x in EXCLUIR):
+                    i += 2
+                    continue
+
+                salida.append(extinf)
+                salida.append(enlace)
+
+                i += 2
+
+            else:
+                i += 1
 
     except Exception as e:
         print(f"Error: {e}")
