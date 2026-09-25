efb4fa36034ce8c617dda508f467ae804acff169
diuh11 <arroflin3@gmail.com>
2026-09-25 12:33:49 +0200

    Fix URL formatting in merge.py

--- merge.py
+++ merge.py
@@ -6,7 +6,7 @@ from collections import Counter
 URLS = [
     "https://pastebin.com/raw/hV2z2e9g",
     "https://raw.githubusercontent.com/Free-TV/IPTV/refs/heads/master/playlists/playlist_spain.m3u8",
-    "https://www.apsattv.com/rakuten_es.m3u"
+    "https://www.apsattv.com/rakuten_es.m3u",
     "https://raw.githubusercontent.com/minhtienth15/mynote/main/t04.m3u"
 ]
 
